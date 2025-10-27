01321f5ba0cfd4acf15e2d99c4bdd9b1be55ea72
himanshuhimmy <122083033+himanshuhimmy@users.noreply.github.com>
2025-10-27 12:22:07 +0530

    ..

--- Blog Mini_Project/Blogs.js
+++ Blog Mini_Project/Blogs.js
@@ -1,25 +1,42 @@
 let mongoose = require(`mongoose`);
 
+let AuthorSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    insta: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  { collection: `authors` }
+);
+
+let AuthorModel = mongoose.model(`authors`, AuthorSchema);
+
 let BlogsSchema = new mongoose.Schema(
   {
-    title: "string",
-    author: "string",
-    authorRating: "number",
-    publishDate: { type: Date },
-    genre: "string",
-    content: "string",
-    researchedFrom: ["string"],
-    tags: ["string"],
-    likes: "number",
-    views: "number",
-    commentsCount: "number",
-    image: {
-      type: String,
+    title: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "authors", //  Foreign key reference
+      required: true,
     },
+    authorRating: { type: Number, default: 0 },
+    publishDate: { type: Date, default: Date.now },
+    genre: { type: String, default: "" },
+    content: { type: String, default: "" },
+    researchedFrom: { type: [String], default: [] },
+    tags: { type: [String], default: [] },
+    likes: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
+    commentsCount: { type: Number, default: 0 },
+    image: { type: String, required: true, default: "" },
   },
   { collection: `NewBlogs` }
 );
-
 let BlogsModel = mongoose.model(`NewBlogs`, BlogsSchema);
 
-module.exports = BlogsModel;
+module.exports = {
+  BlogsModel,
+  AuthorModel,
+};
--- Blog Mini_Project/Index.js
+++ Blog Mini_Project/Index.js
@@ -1,20 +1,22 @@
 require(`./Config`);
 let express = require(`express`);
 let mongoose = require(`mongoose`);
-let blogs = require(`./Blogs`);
+let { BlogsModel, AuthorModel } = require(`./Blogs`);
 let cors = require("cors");
+let multer = require("multer");
+const path = require("path");
 
 let app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get(`/get`, async (req, resp) => {
-  let data = await blogs.find();
+  let data = await BlogsModel.find().populate("author");
   resp.send(data);
 });
 
 app.get(`/get/:id`, async (req, resp) => {
-  let data = await blogs.find({ _id: req.params.id });
+  let data = await BlogsModel.find({ _id: req.params.id }).populate("author");
   resp.send(data);
 });
 
@@ -22,38 +24,98 @@ app.get(`/get/:id`, async (req, resp) => {
 // ! because url doesnot accept sapces to avoid this problem
 
 app.get(`/get/author/:author`, async (req, resp) => {
-  let data = await blogs.find({ author: req.params.author });
+  let data = await BlogsModel.find({ author: req.params.author }).populate(
+    "author"
+  );
   resp.send(data);
 });
 
 app.get(`/get/genre/:genre`, async (req, resp) => {
-  let data = await blogs.find({ genre: req.params.genre });
+  let data = await BlogsModel.find({ genre: req.params.genre }).populate(
+    "author"
+  );
   resp.send(data);
 });
 
 app.get(`/get/:title`, async (req, resp) => {
-  let data = await blogs.find({ title: req.params.title });
+  let data = await BlogsModel.find({ title: req.params.title }).populate(
+    "author"
+  );
   resp.send(data);
 });
 
 app.put(`/update/:id`, async (req, resp) => {
-  let data = await blogs.updateOne(
+  let data = await BlogsModel.updateOne(
     { _id: new mongoose.Types.ObjectId(req.params.id) },
     { $set: req.body }
   );
   resp.send(data);
 });
 
-app.post(`/post`, async (req, resp) => {
-  let data = new blogs(req.body);
+// ! multer setup (Uploading a file)
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "uploads"));
+  },
+  filename: function (req, file, cd) {
+    const UniqueName = Date.now() + path.extname(file.originalname);
+    cd(null, file.fieldname + "-" + UniqueName);
+  },
+});
+const upload = multer({ storage });
+app.use("/uploads", express.static("uploads"));
+
+app.post(`/post`, upload.single("BlogProfile"), async (req, resp) => {
+  const imagePath = req.file
+    ? `http://localhost:7000/uploads/${req.file.filename}`
+    : null;
+
+  let data = new blogs({ ...req.body, image: imagePath });
   let response = await data.save();
   resp.send(response);
-  console.log(data);
 });
 
 app.delete(`/delete/:id`, async (req, resp) => {
-  let data = await blogs.deleteOne({ _id: req.params.id });
+  let data = await BlogsModel.deleteOne({ _id: req.params.id });
   resp.send(data);
 });
+
+//! for Authors
+app.get("/fetchAuthors", async (req, resp) => {
+  let data = await AuthorModel.find();
+  resp.send(data);
+});
+
+app.get("/fetchAuthorId/:id", async (req, resp) => {
+  let data = await AuthorModel.find({ _id: req.params.id });
+  resp.send(data);
+});
+
+app.put("/updateAuthor/:id", async (req, resp) => {
+  let data = await AuthorModel.updateOne(
+    { _id: new mongoose.Types.ObjectId(req.params.id) },
+    { $set: req.body }
+  );
+  resp.send(data);
+});
+
+app.delete("/deleteAuthor/:id", async (req, resp) => {
+  let data = await AuthorModel.deleteOne({ _id: req.params.id });
+  resp.send(data);
+});
+
+app.post(
+  "/addingNewAuthor",
+  upload.single("AuthorProfile"),
+  async (req, resp) => {
+    const imgPath = req.file
+      ? `http://localhost:7000/uploads/${req.file.filename}`
+      : null;
+
+    let data = new AuthorModel({ ...req.body, image: imgPath });
+    let result = await data.save();
+    resp.send(result);
+  }
+);
 
 app.listen(7000);
